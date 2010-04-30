d37e40d631715cf926850d5d8fc82d0ca06340c2
luethib <luethib@dcddef73-2b9d-49fe-a651-d05a6fa2f102>
2010-04-30 08:18:51 +0000

    If fpt_version==4 when Demo is run to look at "image movies" of sequence, now also blue dots are painted to show where the target files "see" 2d particles.
    git-svn-id: https://svn.origo.ethz.ch/ptv/trunk@59 dcddef73-2b9d-49fe-a651-d05a6fa2f102

--- src_c/demo.c
+++ src_c/demo.c
@@ -17,7 +17,7 @@ Routines contained:    	flow_demo_c
 
 int flow_demo_c (ClientData clientData, Tcl_Interp* interp, int argc, const char** argv)
 {
-  int i, i_seq, nr;
+  int i, i_seq, nr, j,pft_version=3;
   char	        name[128];
   unsigned char	*imgf;
   Tk_PhotoHandle img_handle;
@@ -36,9 +36,14 @@ int flow_demo_c (ClientData clientData, Tcl_Interp* interp, int argc, const char
   /* allocate memory */
   imgf = (unsigned char *) calloc (imgsize, 1);
 
+  fpp = fopen ("parameters/pft_version", "r");
+  if (fpp){
+      fscanf (fpp, "%d\n", &pft_version);
+      fclose (fpp);
+  }
+
   /* load and display images */
-  for (i_seq=seq_first; i_seq<=seq_last; i_seq++)
-    {
+  for (i_seq=seq_first; i_seq<=seq_last; i_seq++){
       compose_name_plus_nr (seq_name[nr], "", i_seq, name);
       fp1 = fopen_r (name);	if (! fp1)	return TCL_OK;
       sprintf (buf, "display camera %d, image %d", nr+1, i_seq);
@@ -54,6 +59,34 @@ int flow_demo_c (ClientData clientData, Tcl_Interp* interp, int argc, const char
 
       sprintf(buf, "newimage %d", nr+1);
       Tcl_Eval(interp, buf);
+
+      
+	  if(pft_version==4){
+         sprintf (filename, "%s%s", name,"_targets");
+	      /* read targets of camera nr*/
+	     nt4[3][nr]=0;
+
+	     fp1= fopen (filename, "r");
+	     if (! fp1) printf("Can't open ascii file: %s\n", filename);
+
+         fscanf (fp1, "%d\n", &nt4[3][nr]);
+         for (j=0; j<nt4[3][nr]; j++){
+	          fscanf (fp1, "%4d %lf %lf %d %d %d %d %d\n",
+		      &pix[nr][j].pnr, &pix[nr][j].x,
+		      &pix[nr][j].y, &pix[nr][j].n ,
+		      &pix[nr][j].nx ,&pix[nr][j].ny,
+		      &pix[nr][j].sumg, &pix[nr][j].tnr);
+	     }
+         fclose (fp1);
+
+	     num[nr] = nt4[3][nr];
+
+		 if (display){
+	         for (j=0; j<num[nr]; j++){
+	             drawcross (interp, (int) pix[nr][j].x, (int) pix[nr][j].y,cr_sz, nr, "blue");
+	         }
+		 }
+	  }
       Tcl_Eval(interp, "update idletasks");
     }
 
